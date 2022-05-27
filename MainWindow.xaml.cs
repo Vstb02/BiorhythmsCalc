878becd7e5b7942b216013270a9af3da6402da1f
Vstb02 <v.steblev02@gmail.com>
2022-05-27 22:22:20 +0300

    Добавил верстку

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BiorhythmsCalc.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace BiorhythmsCalc
         public MainWindow()
         {
             InitializeComponent();
+
+            frame.Navigate(new MainView());
         }
     }
 }
